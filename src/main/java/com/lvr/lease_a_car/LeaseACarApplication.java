fe2550465edcf49636194762f02de1b878c2cd2b
Laila <175353500+lvanrooijen@users.noreply.github.com>
2025-07-07 11:00:58 +0200

    chore: init project

---
+++ src/main/java/com/lvr/lease_a_car/LeaseACarApplication.java
@@ -0,0 +1,13 @@
+package com.lvr.lease_a_car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaseACarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaseACarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lvr/lease_a_car/LeaseACarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvr.lease_a_car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaseACarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
